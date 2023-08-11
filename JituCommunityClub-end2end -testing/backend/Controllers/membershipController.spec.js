810616bd919bf6d5d7587f061f3ebf6f85e292e9
Calvince Ndemo <calyndemo16@gmail.com>
2023-08-11 11:46:56 +0300

    modified first test

---
+++ JituCommunityClub-end2end -testing/backend/Controllers/membershipController.spec.js
@@ -0,0 +1,10 @@
+import mssql from 'mssql'
+
+describe('membership controller', ()=>{
+    describe('Add a new member successfully', ()=>{
+        it('should add a new member in a success', async()=>{
+
+        })
+    })
+     
+})
